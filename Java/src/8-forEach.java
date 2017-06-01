13c7a6040edcec2a1d8d791783436ce280cbfaaf
sergfrolov <sergey.frolov.main@gmail.com>
2017-06-01 23:22:23 +0300

    Added Java examples
    Added Java examples

---
+++ Java/src/8-forEach.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+class ForEach {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(7, 10, 1, 5, 2);
+        list.forEach(item -> System.out.println(String.format("%s, %s, %s",item,list.indexOf(item),list.toString())));
+        System.out.println();
+        list.forEach(System.out::print);
+        System.out.println();
+        list.forEach(x -> System.out.print(x));
+    }
+}
