25047c4e06ae105ce0caad3458e3ffddc1559f08
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-05 16:04:55 +0200

    Add functional recursive reduce

--- JavaScript/e-reduce.js
+++ JavaScript/e-reduce.js
@@ -5,3 +5,9 @@ const arr = [7, 10, 1, 5, 2];
 const sum = (acc, val) => (count++, acc + val);
 const res = arr.reduce(sum);
 console.log({ res, count });
+
+const reduce = (fn, acc, [cur, ...rest]) => cur === undefined ?
+  acc : reduce(fn, fn(acc, cur), rest);
+
+const res2 = reduce(sum, 0, arr);
+console.log({ res2 });
