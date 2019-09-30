a035a57a333b5575bc523fea50ea33e3a43d7074
Vlad Esin <44160260+svetley1@users.noreply.github.com>
2019-09-30 13:23:33 +0500

    Пример для наследуемых полей (for...in arr)

--- JavaScript/5-for-in-array.js
+++ JavaScript/5-for-in-array.js
@@ -6,6 +6,8 @@ numbers[-10] = 'Value3';
 numbers.field1 = 'Value1';
 numbers[5] = 20;
 
+Object.prototype.inheritedProperty = 'inherited';
+
 for (const i in numbers) {
   const value = numbers[i];
   console.log(i, typeof i, value);
