a7b5ada06979341a47c3882150547b5c22c90392
Vlad Esin <44160260+svetley1@users.noreply.github.com>
2019-09-30 12:54:21 +0500

    Пример для не enumerable полей

--- JavaScript/5-for-in-array.js
+++ JavaScript/5-for-in-array.js
@@ -5,6 +5,10 @@ numbers.field2 = 'Value2';
 numbers[-10] = 'Value3';
 numbers.field1 = 'Value1';
 numbers[5] = 20;
+Object.defineProperty(numbers, 'newField', {
+    enumerable: false,
+    value: 'valueOfNewField'
+});
 
 for (const i in numbers) {
   const value = numbers[i];
