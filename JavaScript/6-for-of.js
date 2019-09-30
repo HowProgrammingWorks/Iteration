4bdf53bc15661b100d7bd421cb2ff01b093b8910
Vlad Esin <44160260+svetley1@users.noreply.github.com>
2019-09-30 17:19:53 +0500

    Пример с кастомным  @@iterator для for...of
    Пример для объяснения, что 
    1) for...of работает с сущностями у которых есть метод @@iterator, то есть с перечисляемыми
    2) @@iterator можно задавать кастомный и он так же будет работать с for...of

--- JavaScript/6-for-of.js
+++ JavaScript/6-for-of.js
@@ -9,3 +9,35 @@ numbers[5] = 20;
 for (const value of numbers) {
   console.log(value, typeof value);
 }
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr[Symbol.iterator] = function() {
+  let index;
+  const step = 2;
+  return {
+    next: () => {
+      if (index === undefined) {
+        index = 0;
+        return {
+           value: this[index], 
+           done: false,
+        }
+      }
+      if (index === this.length - 1 || index === this.length - 2) {
+        return {
+          value: this[index], 
+          done: true,
+        }
+      } 
+      index += step;
+      return {
+         value: this[index], 
+         done: false,
+      }
+    }
+  }
+}
+
+for (value of arr) {
+  console.log(value);
+}
