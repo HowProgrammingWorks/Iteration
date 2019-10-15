d5382ec38019802da3ad19b996366f1137dbf576
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-10-15 10:50:02 +0300

    Add exercises

---
+++ Exercises/5-reduce.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const sum = (...args) => 0;
+// Use Array.prototype.reduce method
+// to calculate sum of all given arguments
+// For example sum(1, 2, 3) should return 6
+
+module.exports = { sum };
