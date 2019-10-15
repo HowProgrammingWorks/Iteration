5600cc15d95abb8536c588060599ea9625f69125
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-10-15 13:04:11 +0300

    Add for..in task

---
+++ Exercises/7-ages.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const ages = persons => {
+  // Use for..in to calculate age for each person
+  // For example ages({
+  //   lenin: { born: 1870, died: 1924 },
+  //   mao: { born: 1893, died: 1976 },
+  //   gandhi: { born: 1869, died: 1948 },
+  //   hirohito: { born: 1901, died: 1989 },
+  // })
+  // should return {
+  //   lenin: 54,
+  //   mao: 83,
+  //   gandhi: 79,
+  //   hirohito: 88,
+  // }
+};
+
+module.exports = { ages };
