67bb9911a4ae1d54ed0908e178dd3d39abe2cfc2
lizardlynx <Gorbunova.Elizaveta@gmail.com>
2019-10-20 14:21:53 +0300

    All Labs Done

--- Exercises/2-for-of.js
+++ Exercises/2-for-of.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
 module.exports = { sum };
+
+// Use for..of loop and accumulator variable
+// to calculate sum of all given arguments
+// For example sum(1, 2, 3) should return 6
--- Exercises/5-reduce.js
+++ Exercises/5-reduce.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const sum = (...args) => 0;
+const sum = (...args) => args.reduce((s, c) => s += c, 0);
+
+module.exports = { sum };
+
 // Use Array.prototype.reduce method
 // to calculate sum of all given arguments
 // For example sum(1, 2, 3) should return 6
-
-module.exports = { sum };
