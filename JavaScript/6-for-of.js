c8a52c19a9a53a415f46534b902a12388f8b13e7
Vlad Esin <44160260+svetley1@users.noreply.github.com>
2021-02-18 14:06:06 +0500

    Пример с кастомным  @@iterator для for...of (#15)
    * Example with a custom @@iterator for for...of
    
    Example is for explanation that:
    1) for...of works with entities which have the method @@iterator e.g. enumerable entities
    2) @@iterator can be custom and it still will work with for...of

--- JavaScript/6-for-of.js
+++ JavaScript/6-for-of.js
@@ -9,3 +9,27 @@ numbers[5] = 20;
 for (const value of numbers) {
   console.log(value, typeof value);
 }
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr[Symbol.iterator] = function() {
+  let index = 0;
+  const step = 2;
+  return {
+    next: () => {
+      const result = {
+        value: this[index],
+        done: false
+      };
+      if (index >= this.length) {
+        result.done = true;
+        return result;
+      }
+      index += step;
+      return result;
+    }
+  };
+};
+
+for (const value of arr) {
+  console.log(value);
+}
