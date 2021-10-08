20044a3ce12ba82c9da685b919ef8dc313ea7a44
yarman1 <gordienko045@gmail.com>
2021-10-09 00:09:29 +0300

    lab4

--- Exercises/2-for-of.js
+++ Exercises/2-for-of.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let sum = 0;
+  for (const element of args) {
+    sum += element;
+  }
+  return sum;
 };
 
 module.exports = { sum };
--- Exercises/3-while.js
+++ Exercises/3-while.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let accum = 0, i = 0;
+  while (i < args.length) {
+    accum += args[i];
+    i++;
+  }
+  return accum;
 };
 
 module.exports = { sum };
