5849ea18242eca5cc6be71dc903521ff964d23ba
Grevcev.AS <grevcev.as@dns-shop.ru>
2021-10-27 07:16:15 +1000

    lab iteration

--- Exercises/7-ages.js
+++ Exercises/7-ages.js
@@ -1,19 +1,13 @@
 'use strict';
 
 const ages = persons => {
-  // Use for..in to calculate age for each person
-  // For example ages({
-  //   lenin: { born: 1870, died: 1924 },
-  //   mao: { born: 1893, died: 1976 },
-  //   gandhi: { born: 1869, died: 1948 },
-  //   hirohito: { born: 1901, died: 1989 },
-  // })
-  // should return {
-  //   lenin: 54,
-  //   mao: 83,
-  //   gandhi: 79,
-  //   hirohito: 88,
-  // }
+  let ages = {};
+  for (let name in persons) {
+    const person = persons[name];
+    ages[name] = person.died - person.born;
+  }
+
+  return ages;
 };
 
 module.exports = { ages };
