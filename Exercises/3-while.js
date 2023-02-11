ff8fcd3f5bd31a1f6953370111fc9eb74b89135e
Tima Gixe <timagixe@protonmail.com>
2023-02-11 17:33:19 +0200

    Solves 3-while

--- Exercises/3-while.js
+++ Exercises/3-while.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let idx = 0;
+  let res = 0;
+  while (idx < args.length) {
+    res += args[idx];
+    idx++;
+  }
+  return res;
 };
 
 module.exports = { sum };
