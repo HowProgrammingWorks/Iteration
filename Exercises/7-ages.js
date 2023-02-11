9e50956a7710691046280a61fec59a2194389ec8
Tima Gixe <timagixe@protonmail.com>
2023-02-11 17:33:49 +0200

    Solves 7-ages

--- Exercises/7-ages.js
+++ Exercises/7-ages.js
@@ -1,19 +1,13 @@
 'use strict';
 
-const ages = persons => {
-  // Use for..in to calculate age for each person
-  // For example ages({
-  //   lenin: { born: 1870, died: 1924 },
-  //   mao: { born: 1893, died: 1976 },
-  //   gandhi: { born: 1869, died: 1948 },
-  //   hirohito: { born: 1901, died: 1989 },
-  // })
-  // should return {
-  //   lenin: 54,
-  //   mao: 83,
-  //   gandhi: 79,
-  //   hirohito: 88,
-  // }
+const ages = (persons) => {
+  const result = {};
+  for (const name in persons) {
+    const { died, born } = persons[name];
+    const age = died - born;
+    result[name] = age;
+  }
+  return result;
 };
 
 module.exports = { ages };
