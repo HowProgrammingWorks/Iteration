d24ca171d8a0456d843d6529cc241fabac46313a
Tima Gixe <timagixe@protonmail.com>
2023-02-11 17:33:44 +0200

    Solves 6-matrix

--- Exercises/6-matrix.js
+++ Exercises/6-matrix.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const max = matrix => {
-  // Use nested for loop to find max value in 2d matrix
-  // For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-  // should return 9
+const max = (matrix) => {
+  let max = -Infinity;
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    for (let j = 0; j < row.length; j++) {
+      const num = row[j];
+      max = Math.max(max, num);
+    }
+  }
+  return max;
 };
 
 module.exports = { max };
