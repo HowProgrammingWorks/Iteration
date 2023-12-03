d07a62b7569385d816ecb916b5fd3bf2032f0813
Mykhailo Chubko <107486903+reasonkxx@users.noreply.github.com>
2023-12-03 21:29:42 +0200

    Ik-12 Chubko All labwork from 1 to 7

--- Exercises/3-while.js
+++ Exercises/3-while.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let total = 0; 
+  let i = 0;
+
+  while (i < args.length) { 
+    total += args[i]; 
+    i++; 
+  }
+
+  return total; 
 };
 
 module.exports = { sum };
--- Exercises/5-reduce.js
+++ Exercises/5-reduce.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const sum = (...args) => 0;
-// Use Array.prototype.reduce method
-// to calculate sum of all given arguments
-// For example sum(1, 2, 3) should return 6
+const sum = (...args) => {
+    return args.reduce((total, current) => total + current, 0);
+  };
 
 module.exports = { sum };
